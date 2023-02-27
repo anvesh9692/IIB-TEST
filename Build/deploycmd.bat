31e7d453ea0317aca14f6d0629a1f74b8a2eab8e
anvesh9692 <118050289+anvesh9692@users.noreply.github.com>
2023-02-27 17:18:49 +0530

    TEST

--- Build/deploycmd.bat
+++ Build/deploycmd.bat
@@ -1,3 +1,22 @@
-@echo off
+@REM Windows batch file to set the mqsi environment
+@REM and execute the mqsiprofile command.
+
+@REM Saves the current working directory and enter the directory where 'mqsiprofile' generally lives.
+
+@pushd %1
+
 @call "mqsiprofile.cmd" >NUL 2>NUL
-mqsideploy %1 -e %2 -a %3 -w %4
+
+:: Get back to the project directory
+
+@popd
+
+:: Now mqsideploy can be called.
+
+::
+:: %2 = integration node
+:: %3 = intergration server (execution group)
+:: %4 = bar file
+:: %5 = timeout Secs
+
+mqsideploy %2 -e %3 -a %4 -w %5 
