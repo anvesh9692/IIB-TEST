4b4ec8bdaa6450d8eaf67e74a358aed7e5fd4273
anvesh9692 <118050289+anvesh9692@users.noreply.github.com>
2023-07-03 15:54:16 +0530

    Update the batch file

--- Build/Batch_Bar_Version_Updater.bat
+++ Build/Batch_Bar_Version_Updater.bat
@@ -1,25 +1,44 @@
 @echo off
-Set AppName=APP_TEST
-set BarName=BAR_%AppName%
-Set WorkDir=C:\Users\Admin\AppData\Local\Jenkins\.jenkins\workspace\IIB-TEST\Bars
-Set KeywordsDir=C:\Users\Admin\AppData\Local\Jenkins\.jenkins\workspace\IIB-TEST\Build
+::set LOGFILE=D:\Work\ACE12\batch.log
+::call :LOG > %LOGFILE%
+::exit /B
 
-echo Appliction Name - %AppName%
-echo Bar File Name - %BarName%
-@echo Working Directory - %WorkDir%
-copy %WorkDir%\DEV\%BarName%.bar %WorkDir%\DEV\%BarName%.zip
-powershell Expand-Archive -Path %WorkDir%\DEV\%BarName%.zip -DestinationPath %WorkDir%\TempBar
-copy %WorkDir%\TempBar\%AppName%.appzip %WorkDir%\TempBar\%AppName%.zip
-del %WorkDir%\TempBar\%AppName%.appzip
-powershell Expand-Archive -Path %WorkDir%\TempBar\%AppName%.zip -DestinationPath %WorkDir%\TempBar\%AppName%
-del %WorkDir%\TempBar\%AppName%.zip
-copy %KeywordsDir%\keywords.txt %WorkDir%\TempBar\%AppName%\META-INF\keywords.txt
-powershell Compress-Archive -Path %WorkDir%\TempBar\%AppName%\* -DestinationPath %WorkDir%\TempBar\%AppName%.zip
-rmdir /s /q %WorkDir%\TempBar\%AppName%
-copy %WorkDir%\TempBar\%AppName%.zip %WorkDir%\TempBar\%AppName%.appzip
-del %WorkDir%\TempBar\%AppName%.zip
-powershell Compress-Archive -Path %WorkDir%\TempBar\* -DestinationPath %WorkDir%\TempBar\%BarName%.zip
-copy %WorkDir%\TempBar\%BarName%.zip %WorkDir%\DEV\%BarName%.bar
-rmdir /s /q %WorkDir%\TempBar
-del %WorkDir%\DEV\%BarName%.zip
-echo Version update for %BarName%.bar is successful
+::LOG
+set FILE_PATH=C:\Users\Admin\AppData\Local\Jenkins\.jenkins\workspace\IIB-TEST\Build\build.properties
+
+for /f "usebackq tokens=1,2 delims==" %%A in ("%FILE_PATH%") do (
+    if "%%A"=="application_name" (
+        set "APPLICATION_NAME=%%B"
+    )
+	if "%%A"=="workspace_dir" (
+        set "WORKSPACE_DIR=%%B"
+    )
+)
+for %%I in ("%WORKSPACE_DIR%") do (
+    set "REPLACED_DIR=%%~I"
+)
+
+set "REPLACED_DIR=%REPLACED_DIR:/=\%"
+Set KEYWORDS_DIR=%REPLACED_DIR%\Build
+
+echo Application Name: %APPLICATION_NAME%
+echo Workspace Dir: %REPLACED_DIR%
+echo BarFile Name :BAR_%APPLICATION_NAME%
+
+copy %REPLACED_DIR%\Bars\DEV\BAR_%APPLICATION_NAME%.bar %REPLACED_DIR%\Bars\DEV\BAR_%APPLICATION_NAME%.zip
+powershell Expand-Archive -Path %REPLACED_DIR%\Bars\DEV\BAR_%APPLICATION_NAME%.zip -DestinationPath %REPLACED_DIR%\Bars\TempBar
+copy %REPLACED_DIR%\Bars\TempBar\%APPLICATION_NAME%.appzip %REPLACED_DIR%\Bars\TempBar\%APPLICATION_NAME%.zip
+del %REPLACED_DIR%\Bars\TempBar\%APPLICATION_NAME%.appzip
+powershell Expand-Archive -Path %REPLACED_DIR%\Bars\TempBar\%APPLICATION_NAME%.zip -DestinationPath %REPLACED_DIR%\Bars\TempBar\%APPLICATION_NAME%
+del %REPLACED_DIR%\Bars\TempBar\%APPLICATION_NAME%.zip
+copy %KEYWORDS_DIR%\keywords.txt %REPLACED_DIR%\Bars\TempBar\%APPLICATION_NAME%\META-INF\keywords.txt
+powershell Compress-Archive -Path %REPLACED_DIR%\Bars\TempBar\%APPLICATION_NAME%\* -DestinationPath %REPLACED_DIR%\Bars\TempBar\%APPLICATION_NAME%.zip
+rmdir /s /q %REPLACED_DIR%\Bars\TempBar\%APPLICATION_NAME%
+copy %REPLACED_DIR%\Bars\TempBar\%APPLICATION_NAME%.zip %REPLACED_DIR%\Bars\TempBar\%APPLICATION_NAME%.appzip
+del %REPLACED_DIR%\Bars\TempBar\%APPLICATION_NAME%.zip
+powershell Compress-Archive -Path %REPLACED_DIR%\Bars\TempBar\* -DestinationPath %REPLACED_DIR%\Bars\TempBar\BAR_%APPLICATION_NAME%.zip
+copy %REPLACED_DIR%\Bars\TempBar\BAR_%APPLICATION_NAME%.zip %REPLACED_DIR%\Bars\DEV\BAR_%APPLICATION_NAME%.bar
+rmdir /s /q %REPLACED_DIR%\Bars\TempBar
+del %REPLACED_DIR%\Bars\DEV\BAR_%APPLICATION_NAME%.zip
+
+echo Version update for BAR_%APPLICATION_NAME%.bar is successful
