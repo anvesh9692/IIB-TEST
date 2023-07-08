c3b204d758ee5d4100f7a13c91ba8a9a2d3dee37
anvesh9692 <118050289+anvesh9692@users.noreply.github.com>
2023-07-08 12:33:10 +0530

    Updated version update script

--- Build/Batch_Bar_Version_Updater.bat
+++ Build/Batch_Bar_Version_Updater.bat
@@ -25,20 +25,17 @@ echo Application Name: %APPLICATION_NAME%
 echo Workspace Dir: %REPLACED_DIR%
 echo BarFile Name :BAR_%APPLICATION_NAME%
 
-copy %REPLACED_DIR%\Bars\DEV\BAR_%APPLICATION_NAME%.bar %REPLACED_DIR%\Bars\DEV\BAR_%APPLICATION_NAME%.zip
+ren %REPLACED_DIR%\Bars\DEV\BAR_%APPLICATION_NAME%.bar %REPLACED_DIR%\Bars\DEV\BAR_%APPLICATION_NAME%.zip
 powershell Expand-Archive -Path %REPLACED_DIR%\Bars\DEV\BAR_%APPLICATION_NAME%.zip -DestinationPath %REPLACED_DIR%\Bars\TempBar
-copy %REPLACED_DIR%\Bars\TempBar\%APPLICATION_NAME%.appzip %REPLACED_DIR%\Bars\TempBar\%APPLICATION_NAME%.zip
-del %REPLACED_DIR%\Bars\TempBar\%APPLICATION_NAME%.appzip
+ren %REPLACED_DIR%\Bars\TempBar\%APPLICATION_NAME%.appzip %REPLACED_DIR%\Bars\TempBar\%APPLICATION_NAME%.zip
 powershell Expand-Archive -Path %REPLACED_DIR%\Bars\TempBar\%APPLICATION_NAME%.zip -DestinationPath %REPLACED_DIR%\Bars\TempBar\%APPLICATION_NAME%
 del %REPLACED_DIR%\Bars\TempBar\%APPLICATION_NAME%.zip
 copy %KEYWORDS_DIR%\keywords.txt %REPLACED_DIR%\Bars\TempBar\%APPLICATION_NAME%\META-INF\keywords.txt
 powershell Compress-Archive -Path %REPLACED_DIR%\Bars\TempBar\%APPLICATION_NAME%\* -DestinationPath %REPLACED_DIR%\Bars\TempBar\%APPLICATION_NAME%.zip
 rmdir /s /q %REPLACED_DIR%\Bars\TempBar\%APPLICATION_NAME%
-copy %REPLACED_DIR%\Bars\TempBar\%APPLICATION_NAME%.zip %REPLACED_DIR%\Bars\TempBar\%APPLICATION_NAME%.appzip
-del %REPLACED_DIR%\Bars\TempBar\%APPLICATION_NAME%.zip
+ren %REPLACED_DIR%\Bars\TempBar\%APPLICATION_NAME%.zip %REPLACED_DIR%\Bars\TempBar\%APPLICATION_NAME%.appzip
 powershell Compress-Archive -Path %REPLACED_DIR%\Bars\TempBar\* -DestinationPath %REPLACED_DIR%\Bars\TempBar\BAR_%APPLICATION_NAME%.zip
-copy %REPLACED_DIR%\Bars\TempBar\BAR_%APPLICATION_NAME%.zip %REPLACED_DIR%\Bars\DEV\BAR_%APPLICATION_NAME%.bar
+ren %REPLACED_DIR%\Bars\TempBar\BAR_%APPLICATION_NAME%.zip %REPLACED_DIR%\Bars\DEV\BAR_%APPLICATION_NAME%.bar
 rmdir /s /q %REPLACED_DIR%\Bars\TempBar
-del %REPLACED_DIR%\Bars\DEV\BAR_%APPLICATION_NAME%.zip
 
 echo Version update for BAR_%APPLICATION_NAME%.bar is successful
